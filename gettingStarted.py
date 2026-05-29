159df2d903e63c7f4661fda742b26f62caf18c0f
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2026-05-28 23:44:45 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -4,7 +4,7 @@
 
 def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "PASTE_THE_REAL_SLACK_PASSPHRASE_HERE"
+        answer = "pcap"
 
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
